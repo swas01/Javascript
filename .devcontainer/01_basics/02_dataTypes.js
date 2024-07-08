c8fbe96836f417bd6b86b3f109502a54d6c05d9a
swas01 <swastikabasak.uni@gmail.com>
2024-07-08 19:51:44 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as newer version
+
+
+//alert("hello")//We are using nodejs,not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+
+
+console.log("Swastika")
+
+let name="Swastika"//String
+let age=18 //Integer 
+let isLoggedIn=false  //Boolean
+let state;
+
+//number =>2 to the power 53
+//bigint 
+//String=> ""
+//boolean => true/false
+//null => standalone//null is a object
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof "Swastika");//string
+console.log(typeof age);//number
+console.log(typeof undefined);//type of undefined is undefined
+console.log(typeof null);//object
