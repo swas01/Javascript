b204a612be5895fcb146f7028349252453ebf14e
swas01 <swastikabasak.uni@gmail.com>
2024-07-17 08:36:41 +0000

    Array

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//const myArr = [0,1,2,3,4,5,true,"Swastika"] //Different datatypes can be added 
+
+//more elements can be added
+//can have mix datatypes
+//zero based indexing
+/*JavaScript array copy operation create shallow operation
+(All Stadard built in copy operations with array JavaScript object create shallow copies,rather than deep copies)
+*/
+//Array objects cannot be using arbitrary strings
+
+const myarr = [0,1,2,3,4,5,"Swastika"]
+console .log("Swastika")//not allowed
+
